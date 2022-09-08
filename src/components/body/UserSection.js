e7a5d4afe7bc3a59700a077b1f5a5751df046fe3
Fahim Faisal <fahimfaisal1998@gmail.com>
2022-09-09 00:28:29 +0600

    Make responsive done

--- src/components/body/UserSection.js
+++ src/components/body/UserSection.js
@@ -1,24 +1,26 @@
+import { userDummyApi } from "../../api/userPosts"
+
 const UserSection = () => {
     const sugestedUser = [
         {
             name: "harriet12",
-            id:41
+            id: 41
         },
         {
             name: "melvin_star",
-            id:42
+            id: 42
         },
         {
             name: "diane",
-            id:43
+            id: 43
         },
         {
             name: "maxwell",
-            id:44
+            id: 44
         },
         {
             name: "joeraj",
-            id:45
+            id: 45
         }
     ]
 
@@ -27,10 +29,10 @@ const UserSection = () => {
             <div className="row pt-1">
                 <div className="col-12 d-flex justify-content-between align-items-center">
                     <div className="d-flex align-items-center">
-                        <img className="rounded-circle m-2 shadow-sm" alt="profile" height="56px" width="56px" src="https://source.unsplash.com/random/50?sig=10" />
+                        <img className="rounded-circle m-2 shadow-sm" alt="profile" height="56px" width="56px" src={userDummyApi.user.profile_picture} />
                         <div className='mt-1 d-flex flex-column'>
-                            <span className="fw-semibold line-height-2"><small>USernanme</small></span>
-                            <span className="text-muted"><small>Name</small></span>
+                            <span className="fw-semibold line-height-2"><small>{userDummyApi.user.username}</small></span>
+                            <span className="text-muted"><small>{userDummyApi.user.name}</small></span>
                         </div>
                     </div>
                     <div>
--- src/components/body/index.js
+++ src/components/body/index.js
@@ -11,14 +11,14 @@ const Body = () => {
     return (
         <>
             <div style={{ marginTop: "68px", backgroundColor: "#fafafa" }}>
-                <div className="container px-5">
-                    <div className="row gx-4 mx-5 px-5 pt-4">
-                        <div className="col-7">
+                <div className="container-fluid container-lg px-1 px-xl-4 px-xxl-5">
+                    <div className="row mx-1 mx-sm-3 mx-lg-4 mx-xxl-5 px-2 px-lg-3 px-xxl-5 pt-3 pt-md-4">
+                        <div className="col-12 col-md-8 col-xl-7 px-0">
                             <Story />
                             <Posts dummyApi={dummyApi} setDummyApi={setDummyApi} />
                         </div>
-                        <div className="col-5 ps-4">
-                            <UserSection dummyApi={dummyApi}/>
+                        <div className="d-none d-md-block col-4 col-xl-5 ps-4">
+                            <UserSection/>
                         </div>
                     </div>
                 </div>
